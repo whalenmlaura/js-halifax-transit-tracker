c2637d223131e9b920d892b211afab29b81c88b3
Laura Whalen <43071581+whalenmlaura@users.noreply.github.com>
2020-03-06 17:49:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+(function () {
+
+    //create map in leaflet and tie it to the div called 'theMap'
+    var map = L.map('theMap').setView([44.650627, -63.597140], 14);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // define two bus icons
+    var bigBusIcon = L.icon({
+        iconUrl: 'bluebus.png',
+        iconSize: [20] // size of the icon
+    });
+
+    var smallBusIcon = L.icon({
+        iconUrl: 'bluebus.png',
+        iconSize: [10] // size of the icon
+    });
+
+    // convery the JSON to GeoJSON
+    const getGeoJson = (myJson) => {
+        return myJson.map((bus) => {
+            let buses = {
+                type: "Point",
+                routeNum: bus.vehicle.trip.routeId,
+                bearing: bus.vehicle.position.bearing,
+                coordinates: [bus.vehicle.position.longitude, bus.vehicle.position.latitude],
+                speed: bus.vehicle.position.speed
+            };
+            if (typeof (buses.speed) === "undefined") {
+                buses.speed = 0;
+            };
+            return buses;
+        });
+    };
+
+    // define a layer group so that the buses aren't duplicated
+    let busLayer = L.layerGroup().addTo(map);
+
+    // main function to fetch data and filter json
+    const mapBuses = function () {
+        // fetch the bus data, backup API below
+        fetch("https://hrmbuses.herokuapp.com/")
+        //fetch("https://hrmbuses.azurewebsites.net")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                // filter json for bus routes 1-10
+                let routeFilter = myJson.entity.filter(entity => parseInt(entity.vehicle.trip.routeId) <= 10);
+                // clear the layer group
+                busLayer.clearLayers();
+
+                L.geoJSON(getGeoJson(routeFilter), {
+                    onEachFeature: function (feature) {
+                        let longitude = feature.coordinates[0];
+                        let latitude = feature.coordinates[1];
+                        let marker = L.marker([latitude, longitude], { icon: bigBusIcon, rotationAngle: feature.bearing })
+                            .bindPopup(
+                                `Bus Route: ${feature.routeNum}<br/>Speed: ${feature.speed}`
+                            ).addTo(busLayer);
+
+                        // set icon size based on zoom (although it resets every 7 seconds)
+                        map.on('zoomend', function () {
+                            var currentZoom = map.getZoom();
+                            if (currentZoom >= 14) {
+                                marker.setIcon(bigBusIcon);
+                            }
+                            else {
+                                marker.setIcon(smallBusIcon);
+                            }
+                        });
+                    }
+                });
+            });
+        // refresh every 7 seconds    
+        setTimeout(mapBuses, 7000);
+    };
+    // call the function to run everything
+    mapBuses();
+})();
